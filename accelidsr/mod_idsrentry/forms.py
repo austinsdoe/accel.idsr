ca4132a4ada5f579f15880fc9aed32a46b67cbdb
Jordi Puiggené <jpuiggene@naralabs.com>
2017-03-15 19:00:00 +0100

    Fields for Step C

--- accelidsr/mod_idsrentry/forms.py
+++ accelidsr/mod_idsrentry/forms.py
@@ -131,21 +131,34 @@ class IdsrEntryStepCForm(AbstractIdsrEntryStepForm):
     """
     step = 'C'
 
-    # Step A.1
-    reporting_date = DateField('Reporting Date', format='%d/%m/%Y', validators=[DataRequired(), ])
-    facility_code = TextField('Facility Code', validators=[DataRequired(), Length(max=8)])
-    case_id = TextField('Case ID', validators=[Length(max=3), ])
-    patient_record_id = TextField('Patient Record ID', validators=[Length(max=8), ])
+    # Step C.1
+    patient_firstname = TextField('Patient First Name', validators=[DataRequired(), Length(min=3)])
+    patient_middlename = TextField('Patient Middle Name')
+    patient_lastname = TextField('Patient Last Name', validators=[DataRequired(), Length(min=3)])
+    patient_sex = RadioField('Patient Sex',  default='u', choices=[('u', 'Unknown'), ('m', 'Male'), ('f', 'Female')])
+    patient_dateofbirth = DateField('Date of Birth', format='%d/%m/%Y', validators=[DataRequired(), ])
 
-    # Step A.2
-    reporting_country = SelectField('Reporting Country', choices=getCountiesChoices(), validators=[DataRequired(), ])
-    reporting_district = TextField('Reporting District', validators=[DataRequired(), ])
-    facility_name = TextField('Health Facility Name', validators=[DataRequired(), ])
+    # Step C.2
+    patient_county_of_residence = SelectField('County of Residence', choices=getCountiesChoices(), validators=[DataRequired(), ])
+    patient_district_of_residence = SelectField('District of Residence', choices=getDistrictChoices(), validators=[DataRequired(), ])
+    patient_community_of_residence = TextField('Community of Residence', validators=[DataRequired(), Length(min=3)])
+
+    # Step C.3
+    patient_head_of_household = TextField('Head of Household')
+    patient_parents_name = TextField('Patient Parents Name')
+    patient_phone_number = TextField('Patient Phone Number', validators=[Length(max=10), ])
+
+    # Step C.4
+    patient_cross_border = RadioField('Cross Border in Last Month?',  default='u', choices=[('y', 'Yes'), ('n', 'No'), ('u', 'Unknown')])
+    patient_case_detected_community = RadioField('Case Detected at Community Level?', default='u', choices=[('y', 'Yes'), ('n', 'No'), ('u', 'Unknown')])
+
 
     def getSubsteps(self):
         return [
-            [self.reporting_date, self.facility_code, self.case_id, self.patient_record_id],
-            [self.reporting_country, self.reporting_district, self.facility_name]
+            [self.patient_firstname, self.patient_middlename, self.patient_lastname, self.patient_sex, self.patient_dateofbirth],
+            [self.patient_county_of_residence, self.patient_district_of_residence, self.patient_community_of_residence],
+            [self.patient_head_of_household, self.patient_parents_name, self.patient_phone_number],
+            [self.patient_cross_border, self.patient_case_detected_community]
         ]
 
 class IdsrEntryStepDForm(AbstractIdsrEntryStepForm):
