5c53e5db3d29e663ac6249ad94d5b90b9af57832
austinsdoe <austinsdoe@gmail.com>
2017-04-17 09:44:23 +0000

    Added the Plone_site_name as aconfigurable variable to allow different names.  For example, our Bika Plone_site_name is TappitaLab

--- accel.isdr.sync/utils/config.py
+++ accel.isdr.sync/utils/config.py
@@ -12,12 +12,14 @@ if not config.has_section("bika_lims"):
     print("Adding [bika_lims] section to config.ini with default values")
     config.add_section("bika_lims")
     config.set("bika_lims", "plone_site_url", "http://localhost:8080/Plone")
+    config.set("bika_lims", "plone_site_name", "Plone")
     config.set("bika_lims", "jsonapi_username", "admin")
     config.set("bika_lims", "jsonapi_password", "adminsecret")
     config.write(open('config.ini', 'w'))
 
 settings = {
     "plone_site_url": config.get("bika_lims", "plone_site_url").strip('/'),
+    "plone_site_name": config.get("bika_lims", "plone_site_name"),
     "jsonapi_username": config.get("bika_lims", "jsonapi_username"),
     "jsonapi_password": config.get("bika_lims", "jsonapi_password"),
     "db_host": config.get("database", "host"),
