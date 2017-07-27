17bc7237dfd145cf19c58af72c6d1dd3aa718cc1
Nihadness <1992.nihad@gmail.com>
2017-07-27 10:22:52 +0200

    Deleted unnecessary commented lines.

--- accelidsr/start_sync_app.py
+++ accelidsr/start_sync_app.py
@@ -1,19 +1,3 @@
-# import os, sys, inspect
-#
-# cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-# if cmd_folder not in sys.path:
-#    print 'lalall'
-#    sys.path.insert(0, cmd_folder)
-#
-# cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0], "accel_idsr_sync")))
-# if cmd_subfolder not in sys.path:
-#    sys.path.insert(0, cmd_subfolder)
-#
-# import run.Run
-# runner = Run()
-# # runner.start()
-
-
 from accel_idsr_sync.run import Run
 
 runner = Run()
